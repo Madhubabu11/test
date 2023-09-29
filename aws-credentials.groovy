86a1870374fccb491b807c8d8e0f63d8b137fa4c
Madhubabu11 <111720198+Madhubabu11@users.noreply.github.com>
2023-09-29 09:17:22 +0000

    Create aws-credentials.groovy

---
+++ aws-credentials.groovy
@@ -0,0 +1,9 @@
+// aws-credentials.groovy                        
+def publicServerIP = '3.108.236.107'
+def privateServerIP = '172.31.39.73'
+def pub_pvtserverIP = '172.31.3.30'
+def ec2PrivateKeyFile = '/var/lib/jenkins/workspace/download-a-file-from-s3/file.txt/mmkey.pem'
+def sshUser = 'ec2-user'
+def destinationPath = '/home/ec2-user/shiva/'
+def remotefile = '/home/ec2-user/madhu/*'
+def rootuser = 'root'
