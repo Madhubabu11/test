558241f85c31348e9e7a8064a33fe39d33eed637
Madhubabu11 <111720198+Madhubabu11@users.noreply.github.com>
2023-09-29 12:08:24 +0000

    Update aws-credentials.groovy

--- aws-credentials.groovy
+++ aws-credentials.groovy
@@ -1,9 +1,18 @@
-// aws-credentials.groovy                        
-def publicServerIP = '3.108.236.107'
-def privateServerIP = '172.31.39.73'
-def pub_pvtserverIP = '172.31.3.30'
-def ec2PrivateKeyFile = '/var/lib/jenkins/workspace/download-a-file-from-s3/file.txt/mmkey.pem'
-def sshUser = 'ec2-user'
-def destinationPath = '/home/ec2-user/shiva/'
-def remotefile = '/home/ec2-user/madhu/*'
-def rootuser = 'root'
+// AWS credentials
+// Public and private IP addresses
+def publicIpAddress = '3.108.236.107'
+def privateIpAddress = '172.31.39.73'
+def pub_pvtserverIpAddress = '172.31.3.30'
+
+// Key pair details
+def keyPairName = 'mmkey.pem'
+def keyPairPath = '/var/lib/jenkins/workspace/pranav/mmkey.pem'
+
+// Print IP addresses
+println "Public IP Address: $publicIpAddress"
+println "Private IP Address: $privateIpAddress"
+println "pub_pvtserverIpAddress: $pub_pvtserverIpAddress"
+
+// Print key pair details
+println "Key Pair Name: $keyPairName"
+println "Key Pair Path: $keyPairPath"
